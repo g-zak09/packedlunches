b859a715be5df097f9a41b89dc2a82e9d3e05139
Your Name <you@example.com>
2025-12-06 12:22:52 +0000

    chekout

--- checkout.php
+++ checkout.php
@@ -1,20 +1,47 @@
 <?php
-session_start();
-print_r($_SESSION);
-include_once("connection.php");
-$datecheckedout=date_create()->format("Y-m-d H:i:s");
-print_r($datecheckedout);
-$status=("ordered");
-$stmt=$conn->prepare("INSERT INTO tblorder
-    (OrderID, Status, UserID, Orderdate)
+    session_start();
+    print_r($_SESSION);
+    include_once("connection.php");
+    $datecheckedout=date_create()->format("Y-m-d H:i:s");
+    print_r($datecheckedout);
+    $status="ordered";
+    $stmt=$conn->prepare("INSERT INTO tblorder 
+    (OrderID, Status,UserID,Orderdate)
     VALUES
-    (NULL,:status, :userid, :orderdate)
+    (NULL,:status,:userid,:orderdate)
     ");
     $stmt->bindParam(":status", $status);
     $stmt->bindParam(":userid", $_SESSION["loggedinuser"]);
     $stmt->bindParam(":orderdate", $datecheckedout);
+    
     $stmt->execute();
     $lastorderid=$conn->lastInsertId();
     print_r($lastorderid);
-
+    
+    foreach ($_SESSION["lunchbasket"] as $item){
+        #adds each item in lunchbasket to the tblbasket table
+        echo($item["foodid"]);
+        
+        $fid=$item["foodid"];
+        $stmt=$conn->prepare("INSERT INTO tblbasket 
+        (OrderID,Quantity, FoodID)
+        VALUES
+        (:orderid,:qty,:foodid)
+        ");
+        $stmt->bindParam(":orderid", $lastorderid);
+        $stmt->bindParam(":qty", $item["qty"]);
+        $stmt->bindParam(":foodid", $item["foodid"]);
+        
+        $stmt->execute();
+        
+        
+    } 
+    $stmt=$conn->prepare("UPDATE tblusers SET Balance=Balance-:totalcost WHERE UserID=:userid"); 
+       
+    $stmt->bindParam(":totalcost", $_SESSION["totalcost"]);
+    $stmt->bindParam(":userid", $_SESSION["loggedinuser"]);
+        
+    $stmt->execute();
+    unset($_SESSION["lunchbasket"]);#empty the basket array
+    header("location: index.php");
 ?>
